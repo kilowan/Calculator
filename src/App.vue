64241a61e6a631359a2f313bfeca37de149831b1
PC-GAMER\Juan Francisco <kilowan2@gmail.com>
2021-09-12 17:04:34 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,284 +1,176 @@
 <template>
-<login v-if="page == 'Login'" id="Login" @logedIn="logedIn($event)"></login>
-<!-- Menu -->
-<div v-if="page == 'Menu'" id="Menu">
-  <div class="cabecera">
-    <p class="mensaje">Bienvenido {{user.name}} {{user.surname1}} {{user.surname2}}</p>
-    <div class="Logo">
-      <a @click="logOut()" href="./">
-        <img class="cierra" src="./shutdown.png" alt="Cerrar sesión" />
-      </a>
-    </div>
-    <div class="opciones">
-      <a href="#" @click="add('MakeIncidence')" v-if="user.permissions.includes('13')" class="link">Crear parte</a>
-      <a href="#" @click="add('incidences')" v-if="incidencesCount >0" class="link">Ver partes</a>
-      <a href="#" @click="add('statistics')" v-if="user.permissions.includes('2')" class="link" >Estadísticas</a> 
-      <a href="#" @click="add('employeeList')" v-if="user.permissions.includes('16')" class="link">Lista empleados</a>
-      <a href="#" @click="add('user_info')" class="link" >Datos personales</a>
-    </div>
-  </div>
-    <div v-if="check('MakeIncidence')" class="cuerpo">
-      <make-incidence v-if="user" :user="user" @closeForm="mod='Main'" class="mensaje"/>
-    </div>
-    <div v-else-if="check('user_info')" class="cuerpo">
-      <user-info  v-if="user" :username="username" @reloadUser="reloadUser($event)"/>
-    </div>
-    <div v-else-if="check('statistics')" class="cuerpo">
-      <statistics  v-if="user" :user="user"/>
-    </div>
-    <div v-else-if="check('employeeList')" class="cuerpo">
-      <employee-list  v-if="user" :user="user" :incidences="incidences"/>
-    </div>
-    <div v-else-if="check('incidences')" class="cuerpo">
-      <incidences 
-      v-if="user" 
-      :user="user"
-      :reload="reload"
-      :incidences="incidences"
-      @linked="reload=false"
-      @reload="reloading()"
-      />
-    </div>
-    <div v-else class="cuerpo">
-      <p>Not working</p>
-    </div>
-
-    <div class="Pie">
-      <p>Trabajo realizado por Jose Javier Valero Fuentes y Juan Francisco Navarro Ramiro para el curso de ASIR 2º X</p>
-    </div>
-</div>
+		<form v-if="page == 'select'" action="curriculum.php" method="post" >
+			<label>Tipo</label>
+			<input name="tipo" type="text" id="tipo" required>
+			<input @click="page='main'" type="submit" name="Submit" value="LOGIN">
+		</form>
+		<div v-else id="page-wrap">		
+			<img src="./images/foto.jpg" alt="Photo of juan" id="pic" />		
+			<div id="contact-info" class="vcard">			
+				<!-- Microformats! -->			
+				<h1 class="fn">Juan Francisco Navarro Ramiro</h1>			
+				<p>
+					Teléfono: <span class="tel">656995114</span><br />
+					Email: <a class="email" href="mailto:kilowan2@gmail.com">kilowan2@gmail.com</a>
+				</p>
+			</div>					
+			<div id="objective">
+				<p>
+					Busco perfeccionar mis habilidades y si es posible adquirir nuevas y a su vez perfeccionarlas también, además de aumentar a mi experiencia tanto como se pueda, mi estilo es colaborativo más que competitivo pero si hay que competir se compite.
+				</p>
+			</div>			
+			<div class="clear"></div>			
+			<dl>
+				<dd class="clear"></dd>				
+				<dt class="formacion">Formación académica</dt>
+				<dd>
+					<h2><strong>Bachillerato científico-técnico</strong></h2>
+					<ul>
+						<li><strong>Centro:</strong> I.E.S Carrús</li>
+						<li><strong>Graduación:</strong> 2017</li>
+					</ul>
+				</dd>
+				<dd>
+					<h2><strong>Administración de Sistemas Informáticos en Red</strong></h2>
+					<ul>
+						<li><strong>Centro:</strong> I.E.S. Severo Ochoa</li>
+						<li><strong>Graduación:</strong> 2018</li>
+					</ul>
+					<h2>Beca Everis</h2>
+					<ul>
+						<li>Lugar: San Juan (Alicante)</li>
+						<li>Fecha inicio: 01/2019</li>
+						<li>Fecha Fin: 03/2019</li>
+						<li>Contenido:
+							<ul>
+								<li><h5>.net Framework</h5></li>
+								<li><h5>GitHub</h5></li>
+								<li><h5>Sharepoint</h5></li>
+							</ul>
+						</li>
+					</ul>
+				</dd>
+				<dd class="clear"></dd>
+				<dt class="formacion2">Formación complementaria</dt>
+				<dd>
+					<h2>Programación</h2>
+					<ul>
+						<li><h5>Lenguajes</h5>
+							<ul>
+								<li>Javascript</li>
+								<li>Typescript</li>
+								<li>C# (.net Framework, .net Core)</li>
+								<li>PHP</li>
+							</ul>
+						</li>
+						<li><h5>Conocimiento en Frameworks</h5>
+							<ul>
+								<li>Vue.js</li>
+								<li>jquery</li>
+								<li>bootstrap</li>
+							</ul>
+						</li>
+						<li><h5>Tecnologías de microsoft</h5>
+							<ul>
+								<li>WCF</li>
+								<li>WPF</li>
+								<li>Azure Web services</li>
+							</ul>
+						</li>
+						<li>
+							<h5>Bases de datos</h5>
+							<ul>
+								<li>MySql</li>
+								<li>Postgres</li>
+								<li>Sql Server</li>
+							</ul>
+						</li>
+					</ul>
+				</dd>
+				<dd class="clear"></dd>
+				<dt class="experiencia">Experiencia profesional</dt>
+				<dd>
+					<ul>
+						<li>
+							<h2>Grupo oesía </h2>
+							<ul>
+								<li>Desarrollo Backend de webservices .net en c#
+									<ul>
+										<li>Lugar: Murcia</li>
+										<li>Fecha de inicio/ Fin: 09/2019-09/2020</li>
+									</ul>
+								</li>
+								<li>Desarrollo Frontend en Vue.js
+									<ul>
+										<li>Lugar: Murcia</li>
+										<li>Fecha de inicio/ Fin: 09/2010-Actualidad</li>
+									</ul>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</dd>						
+				<dd class="clear"></dd>
+				<dt class="idiomas">Idiomas</dt>
+				<dd>
+					<ul>
+						<li><strong>Inglés:</strong> equivalente a B1</li>
+						<li><strong>Valenciano:</strong> equialente a A2</li>
+						<li><strong>Español:</strong> Nativo</li>
+					</ul>
+				</dd>				
+				<dd class="clear"></dd>					
+				<dt class="otros">Otros datos</dt>
+				<dd>
+					<ul>
+						<li>Disponibilidad horaria</li>
+						<li>Puntualidad, responsabilidad y constancia</li>
+						<li>Incorporación inmediata</li>
+						<li>Disponibilidad de cambio de domicilio</li>
+					</ul>
+				</dd>					
+				<dd class="clear"></dd>
+			</dl>
+			<dd class="clear"></dd>
+		</div>
 </template>
 
 <script>
-import makeIncidence from './components/makeIncidence.vue';
-import axios from 'axios';
-import userInfo from './components/userInfo.vue';
-import login from './components/Login.vue';
-import statistics from './components/statistics.vue';
-import employeeList from './components/employeeList.vue';
-import incidences from './components/incidences.vue';
 
 export default {
   name: 'App',
-  components: {
-    login,
-    makeIncidence,
-    userInfo,
-    statistics,
-    employeeList,
-    incidences,
-  },
-  data:function()
-  {
-    return {
-      page: 'Login',
-      mod: 'Main',
-      user: undefined,
-      incidences: undefined,
-      incidencesCount: 0,
-      reload: false,
-    }
-  },
-  methods: {
-    check: function(data)
-    {
-      return this.mod == data? true: false;
-    },
-    reloading: function()
-    {
-        axios.get("http://localhost:8082/newMenu.php?funcion=getAllincidences")
-        .then( datas => {
-          this.incidences = datas.data;
-          this.reload = true;
-        });
-    },
-    logedIn: function(data)
-    {
-      axios.get("http://localhost:8082/employee.php?funcion=getEmployeeByUsername&username="+ data.username)
-      .then( datas => {
-        this.user = datas.data;
-        this.username = data.username;
-        this.page = 'Menu';
-        axios.get("http://localhost:8082/newMenu.php?funcion=getAllincidences")
-        .then( datas => {
-          this.incidences = datas.data;
-          this.showIncidences();
-        });
-      });
-    },
-    reloadUser: function(data)
-    {
-      axios.get("http://localhost:8082/employee.php?funcion=getEmployeeByUsername&username="+ data)
-      .then( datas => {
-        this.user = datas.data;
-      });
-    },
-    add:function(data)
-    {
-      if (data == 'incidences' && this.mod =='incidences') {
-        this.reloading();
-      }
-      this.mod = data;
-    },
-
-    showIncidences: function()
-    {
-      let new_array = undefined;
-      if(this.user.permissions.includes("7") && this.user.permissions.includes("8") && this.user.permissions.includes("9"))
-      {
-          new_array = this.incidences.filter(array => {
-              return (array.owner.id == this.user.id && array.state != 5);
-          });
-          this.incidencesCount = new_array.length;
-      }
-      else if (this.user.permissions.includes("3") && this.user.permissions.includes("4") && this.user.permissions.includes("5"))
-      {
-          new_array = this.incidences.filter(array => {
-              return (array.solver.id == this.user.id || array.state == 1);
-          });
-          this.incidencesCount = new_array.length;
-      }
-      else if (this.user.permissions.includes("6") && this.user.permissions.includes("7") && this.user.permissions.includes("8") && this.user.permissions.includes("9") && this.user.permissions.includes("10") && this.user.permissions.includes("11") && this.user.permissions.includes("12")) 
-      {
-          new_array = this.incidences.filter(array => {
-              return (array.solver.id == this.user.id || (array.state == 1 || array.state == 2 || array.state == 3 || array.state == 4) || array.owner.id == this.user.id);
-          });
-          this.incidencesCount = new_array.length;
-          this.page = 'Menu';
-      }
-    },
-    logOut:function()
-    {
-      this.page = 'Login';
-      this.mod = 'Main';
-      this.form = {
-        username: undefined,
-        pass: undefined,
-      };
-      this.user = undefined;
-      this.incidences = undefined;
-      this.incidencesCount = 0;
-    }
-  },
-  mounted:function()
-  {}
+  components: {},
+  data:function(){
+		return {
+			page: 'select',
+		}
+	},
+  methods: {},
+  mounted:function(){}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-body
-{
-	background:url("fondo.gif");
-	font-size: 100%;
-	font-family: sans-serif;
-}
-.mensaje
-{
-	text-align: center;
-  font-size: 1.4em;
-  font-weight: bold;
-	color: white;
-	top: 5%;
-	width: 40%;
-	left: 30%;
-	position: relative;
-}
-.Logo
-{
-	text-align: right;
-	right: 5%;
-	top: 10%;
-	position: absolute;
-}
-img.cierra
-{
-	width: 40px; height: 40px;
-}
-.login
-{
-  font-size: 0.9em;
-  right: 1%;
-  top: 9%;
-  position: fixed;
-}
-.opciones
-{
-	text-align: center;
-	font-size: 0.9em;
-	bottom: 0;
-	width: 60%;
-	left: 20%;
-	position: absolute;
-}
-.link
-{
-	color: white;
-  padding-right: 6px;
-}
-.brand
-{
-  color: white;
-  text-align: left;
-}
-.input
-{
-  margin-right: 6px;
-}
-.welcome
-{
-  top: 2%;
-  left: 40%;
-  position: fixed;
-}
-.cuerpo
-{
-	top: 12%;
-	left: 0%;
-	width: 100%;
-	bottom: 10%;
-	position: fixed;
-	overflow: auto;
-}
-table
-{
-	box-shadow: 5px 5px 10px #999;
-	border: 1px solid white;
-    background: white;
-	left: 10%;
-	width: 80%;
-	position: relative;
-	border-spacing: 0px;
-}
-th
-{
-	border: 1px solid white;
-    color: black;
-	background: #d7dee3 url("tabla.gif") repeat-x top left;
-}
-td
-{
-	font-size: 100%;
-	text-align: center;
-	border: 1px dashed gray;
-	color: black;
-}
-.Pie
-{
-	bottom: 0;
-	left: 0;
-	right: 0;
-	height: 10%;
-	position: fixed;
-	background-color: #333;
-	color: white;
-	text-align: center;
-}
+* { margin: 0; padding: 0; }
+body { font: 16px Helvetica, Sans-Serif; line-height: 24px; background: url(./images/noise.jpg); }
+.clear { clear: both; }
+.idiomas { height: 80px; border-right: 1px solid #999; }
+.otros { height: 110px; border-right: 1px solid #999; }
+#page-wrap { width: 800px; margin: 40px auto 60px; }
+#pic { float: right; margin: -30px 0 0 0; height: 100px; }
+h1 { margin: 0 0 16px 0; padding: 0 0 16px 0; font-size: 34px; font-weight: bold; letter-spacing: -2px; border-bottom: 1px solid #999; }
+h2 { font-size: 20px; margin: 0 0 6px 0; position: relative; }
+h2 span { position: absolute; bottom: 0; right: 0; font-style: italic; font-family: Georgia, Serif; font-size: 16px; color: #999; font-weight: normal; }
+p { margin: 0 0 16px 0; }
+a { color: #999; text-decoration: none; border-bottom: 1px dotted #999; }
+a:hover { border-bottom-style: solid; color: black; }
+ul { margin: 0 0 32px 17px; }
+#objective { width: 500px; float: left; }
+#objective p { font-family: Georgia, Serif; font-style: italic; color: #666; }
+dt { font-style: italic; font-weight: bold; font-size: 18px; text-align: right; padding: 0 26px 0 0; width: 150px; float: left; height: 100px; border-right: 1px solid #999;  }
+dd { width: 600px; float: right; }
+dd.clear { float: none; margin: 0; height: 15px; }
+.formacion { height: 450px; border-right: 1px solid #999; }
+.formacion2 { height: 540px; border-right: 1px solid #999; }
+.experiencia{ height: 210px; border-right: 1px solid #999; }
 </style>
