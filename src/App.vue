867df3369aa19627535cd1c9729c0eb5a341d03e
PC-GAMER\Juan Francisco <kilowan2@gmail.com>
2021-09-13 21:15:26 +0200

    update style

--- src/App.vue
+++ src/App.vue
@@ -18,15 +18,15 @@
 				<dd>
 					<ul>
 						<li v-for="(academicTraining, index) in inputData.academicTraining" v-bind:key="index">
-							<h2><strong>{{ alturaFormacion(academicTraining.name) }}</strong></h2>
+							<h2><strong>{{ altura(academicTraining.name, 65, 0) }}</strong></h2>
 							<ul>
-								<li><strong>Centro/ Lugar: </strong>{{ academicTraining.place }}</li>
-								<li v-if="academicTraining.graduationDate"><strong>Graduación: </strong>{{ academicTraining.graduationDate }}</li>
-								<li v-if="academicTraining.initDate"><strong>Fecha inicio: </strong>{{ academicTraining.initDate }}</li>
-								<li v-if="academicTraining.endDate"><strong>Fecha Fin: </strong>{{ academicTraining.endDate }}</li>
-								<li v-if="academicTraining.content">Contenido:
+								<li><strong>Centro/ Lugar: </strong>{{ altura(academicTraining.place, 15, 0) }}</li>
+								<li v-if="academicTraining.graduationDate"><strong>Graduación: </strong>{{ altura(academicTraining.graduationDate, 15, 0) }}</li>
+								<li v-if="academicTraining.initDate"><strong>Fecha inicio: </strong>{{ altura(academicTraining.initDate, 15, 0) }}</li>
+								<li v-if="academicTraining.endDate"><strong>Fecha Fin: </strong>{{ altura(academicTraining.endDate, 15, 0) }}</li>
+								<li v-if="academicTraining.content">{{ altura('Contenido', 10) }}:
 									<ul>
-										<li v-for="(content, contentIndex) in academicTraining.content" v-bind:key="contentIndex">{{ content }}</li>
+										<li v-for="(content, contentIndex) in academicTraining.content" v-bind:key="contentIndex">{{ altura(content, 15, 0) }}</li>
 									</ul>
 								</li>
 							</ul>
@@ -34,16 +34,16 @@
 					</ul>
 				</dd>
 				<dd class="clear"></dd>
-				<dt id="formacion2" style="height: 0px;">Formación complementaria</dt>
-				<dd>
+				<dt id="formacion2" style="height: 0px;" v-if="inputData.complementaryTraining">Formación complementaria</dt>
+				<dd v-if="inputData.complementaryTraining">
 					<ul>
 						<li v-for="(complementaryTraining, firstindex) in inputData.complementaryTraining" v-bind:key="firstindex">
-							<h2>{{ complementaryTraining.name }}</h2>
+							<h2>{{ altura(complementaryTraining.name, 60, 1) }}</h2>
 							<ul>
 								<li v-for="(content, secondindex) in complementaryTraining.content" v-bind:key="secondindex">
-									<h5>{{ alturaFormacion2(content.key) }}</h5>
+									<h5>{{ altura(content.key, 40, 1) }}</h5>
 									<ul>
-										<li v-for="(data, thirdindex) in content.values" v-bind:key="thirdindex">{{ data }}</li>
+										<li v-for="(data, thirdindex) in content.values" v-bind:key="thirdindex">{{ altura(data, 20, 1) }}</li>
 									</ul>
 								</li>
 							</ul>
@@ -51,16 +51,16 @@
 					</ul>
 				</dd>
 				<dd class="clear"></dd>
-				<dt class="experiencia">Experiencia profesional</dt>
-				<dd>
+				<dt style="height: 0px;" id="experiencia" v-if="inputData.professionalExperience">Experiencia profesional</dt>
+				<dd v-if="inputData.professionalExperience">
 					<ul>
 						<li v-for="(professionalExperience, firstindex) in inputData.professionalExperience" v-bind:key="firstindex">
-							<h2>{{ professionalExperience.name }}</h2>
+							<h2>{{ altura(professionalExperience.name, 70, 2) }}</h2>
 							<ul>
-								<li v-for="(contract, firstindex) in professionalExperience.contracts" v-bind:key="firstindex">{{ contract.name }}
+								<li v-for="(contract, firstindex) in professionalExperience.contracts" v-bind:key="firstindex">{{ altura(contract.name, 30, 2) }}
 									<ul>
-										<li>Lugar: {{ contract.place }}</li>
-										<li>Fecha de inicio/ Fin: {{ contract.date }}</li>
+										<li>Lugar: {{ altura(contract.place, 20, 2) }}</li>
+										<li>Fecha de inicio/ Fin: {{ altura(contract.date, 10, 2) }}</li>
 									</ul>
 								</li>
 							</ul>
@@ -200,23 +200,19 @@ export default {
 					}
 				]
 			},
-			height: 100,
-			height2: 0,
+			height: [100, 0, 0],
 		}
 	},
   methods: {
-		alturaFormacion: function(data) {
-			this.height = this.height + 115;
-			return data;
-		},
-		alturaFormacion2: function(data) {
-			this.height2 = this.height2 + 125;
+		altura: function(data, size, id) {
+			this.height[id] = this.height[id] + size;
 			return data;
 		},
   },
   mounted:function(){
-		document.querySelector('#formacion').attributes.style.value = 'height:' + this.height + 'px;';
-		document.querySelector('#formacion2').attributes.style.value = 'height:' + this.height2 + 'px;';
+		document.querySelector('#formacion').attributes.style.value = 'height:' + this.height[0] + 'px;';
+		document.querySelector('#formacion2').attributes.style.value = 'height:' + this.height[1] + 'px;';
+		document.querySelector('#experiencia').attributes.style.value = 'height:' + this.height[2] + 'px;';
   }
 }
 </script>
@@ -227,7 +223,7 @@ body { font: 16px Helvetica, Sans-Serif; line-height: 24px; background: url(./im
 .clear { clear: both; }
 .idiomas { height: 80px; border-right: 1px solid #999; }
 .otros { height: 110px; border-right: 1px solid #999; }
-#page-wrap { width: 800px; margin: 40px auto 60px; }
+#page-wrap { width: 1000px; margin: 40px auto 60px; }
 #pic { float: right; margin: -30px 0 0 0; height: 100px; }
 h1 { margin: 0 0 16px 0; padding: 0 0 16px 0; font-size: 34px; font-weight: bold; letter-spacing: -2px; border-bottom: 1px solid #999; }
 h2 { font-size: 20px; margin: 0 0 6px 0; position: relative; }
@@ -236,12 +232,12 @@ p { margin: 0 0 16px 0; }
 a { color: #999; text-decoration: none; border-bottom: 1px dotted #999; }
 a:hover { border-bottom-style: solid; color: black; }
 ul { margin: 0 0 32px 17px; }
-#objective { width: 500px; float: left; }
+#objective { width: 100%; float: left; }
 #objective p { font-family: Georgia, Serif; font-style: italic; color: #666; }
 dt { font-style: italic; font-weight: bold; font-size: 18px; text-align: right; padding: 0 26px 0 0; width: 150px; float: left; height: 100px; border-right: 1px solid #999;  }
-dd { width: 600px; float: right; }
+dd { width: 800px; float: right; }
 dd.clear { float: none; margin: 0; height: 15px; }
 .formacion { border-right: 1px solid #999; }
 .formacion2 { border-right: 1px solid #999; }
-.experiencia{ height: 210px; border-right: 1px solid #999; }
+.experiencia{ border-right: 1px solid #999; }
 </style>
