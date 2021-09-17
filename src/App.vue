9fb8c17ea75744856eef12a6ed08c44766812c07
PC-GAMER\Juan Francisco <kilowan2@gmail.com>
2021-09-17 21:45:31 +0200

    improve code

--- src/App.vue
+++ src/App.vue
@@ -45,9 +45,6 @@ export default {
 			binValues: ['0', '1'],
 			decValues: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
 			HexValues: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'],
-			decHexValues: {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 'A': '10', 'B': '11', 'C': '12', 'D': '13', 'E': '14', 'F': '15'},
-			hexValues: {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'},
-			hexaBinValues: {'0': '0000', '1': '0001', '2': '0010', '3': '0011', '4': '0100', '5': '0101', '6':'0110', '7':'0111', '8':'1000', '9':'1001', 'A':'1010', 'B':'1011', 'C':'1100', 'D':'1101', 'E':'1110', 'F':'1111'},
 		}
 	},
   methods: {
@@ -99,7 +96,7 @@ export default {
 		while(number > 0) {
 			let resto = number%16;
 			number = Math.trunc(number/16);
-			resultado = this.hexValues[resto] + resultado;
+			resultado = this.HexValues[resto] + resultado;
 		}
 		return resultado;
 	},
