d19ba1de8dcab2cfe2683370312fff51059447a1
PC-GAMER\Juan Francisco <kilowan2@gmail.com>
2021-09-18 16:31:37 +0200

    improve code

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
 					<p>El numero {{ number }} en binario es {{ all? number : deciabin(number) }}</p>
 					<p>El numero {{ number }} en Hexadecimal es {{ all? number : decihexa(parseInt(number, 10)) }}</p>
 				</div>
-				<div v-if="result.includes('hexadecimal')">
+				<div v-if="result.includes(hexadecimal)">
 					<h2><p>Origen hexadecimal:</p></h2>
 					<p>El numero {{ number }} en Binario es {{ all? number : deciabin(parseInt(number, 16)) }}</p>
 					<p>El numero {{ number }} en Decimal es {{ all? number : parseInt(number, 16) }}</p>
@@ -30,6 +30,9 @@
 </template>
 
 <script>
+const hexadecimal = 'hexadecimal';
+const decimal = 'decimal';
+const binario = 'binario';
 
 export default {
   name: 'App',
@@ -41,9 +44,10 @@ export default {
 			result: [],
 			error: '',
 			all: false,
-			binValues: [],
-			decValues: [],
-			HexValues: [],
+			values: {},
+			hexadecimal: hexadecimal,
+			decimal: decimal,
+			binario: binario,
 		}
 	},
   methods: {
@@ -55,45 +59,47 @@ export default {
 	},
 	checkNumber: function(number) {
 		//2 - Comprobaciones
-		if(this.binValues.includes(number)) {
-			this.result.push('binario', 'decimal', 'hexadecimal');
+		if(this.values[binario].includes(number)) {
+			this.result.push(binario, decimal, hexadecimal);
 			this.all = true;
 		} else if(number != "0") {
 			//Binario
-			this.checkNumberTypes(number, this.binValues, 'binario');
+			this.checkNumberTypes(number, binario);
 			//Decimal
-			this.checkNumberTypes(number, this.decValues, 'decimal');
+			this.checkNumberTypes(number, decimal);
 			//Hexadecimal
-			this.checkNumberTypes(number.toUpperCase(), this.HexValues, 'hexadecimal', true);
+			this.checkNumberTypes(number.toUpperCase(), hexadecimal, true);
 		} else this.error += number;
 	},
-	checkNumberTypes: function(number, array, type, last = false) {
+	checkNumberTypes: function(number, type, last = false) {
 		let length = 0;
 		for(let i = 0; i <= number.toString().length-1; i++)	{
-			array.includes(number[i]) ? length++ : last? this.error += number[i] : this.error = '';
+			this.values[type].includes(number[i]) ? length++ : last? this.error += number[i] : this.error = '';
 		}
+
 		if(length == number.toString().length) this.result.push(type);
 	},
 	//changeBase dec to hexa/bin
-	changeBase: function(number, array, base) {
+	changeBase: function(number, type) {
+		let base = {binario: 2, hexadecimal: 16};
 		let resultado = "";
 		while(number > 0) {
-			let resto = number%base;
-			number = Math.trunc(number/base);
-			resultado = array[resto] + resultado;
+			let resto = number%base[type];
+			number = Math.trunc(number/base[type]);
+			resultado = this.values[type][resto] + resultado;
 		}
 
 		return resultado.toString();
 	},
 	//1.1 - Decimal a binario
-	deciabin: function(number) { return this.changeBase(number, this.binValues, 2);	},
+	deciabin: function(number) { return this.changeBase(number, binario);	},
 	//1.4 - Decimal a hexadecimal
-	decihexa: function(number) { return this.changeBase(number, this.HexValues, 16); },
+	decihexa: function(number) { return this.changeBase(number, hexadecimal); },
   },
   mounted:function(){
-	this.binValues = ['0', '1'];
-	this.decValues = this.binValues.concat(['2', '3', '4', '5', '6', '7', '8', '9']);
-	this.HexValues = this.decValues.concat(['A', 'B', 'C', 'D', 'E', 'F']);
+	this.values[binario] = ['0', '1'];
+	this.values[decimal] = this.values[binario].concat(['2', '3', '4', '5', '6', '7', '8', '9']);
+	this.values[hexadecimal] = this.values[decimal].concat(['A', 'B', 'C', 'D', 'E', 'F']);
   }
 }
 </script>
